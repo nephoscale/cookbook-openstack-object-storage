d28a2e257145119f68874f1830786a005f304b89
Alan Meadows <alan.meadows@gmail.com>
2016-01-14 16:12:53 -0800

    bugfix

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'openstack-dev@lists.openstack.org'
 license 'Apache 2.0'
 description 'Installs and configures Openstack Swift'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '12.0.5'
+version '12.0.6'
 
 recipe 'openstack-object-storage::account-server', 'Installs the swift account server'
 recipe 'openstack-object-storage::client', 'Install the swift client'
